4b494732b73abc6253c1b7adf10dffc1e7d893a1
lilycat0612 <27803532@qq.com>
2017-08-09 13:11:56 +0800

    Add sort function in CountDupLines

--- myFirst/CountDupLines/history.py
+++ myFirst/CountDupLines/history.py
@@ -1,5 +1,7 @@
 #coding=utf-8
 import operator
+import xlwt
+
 f = open("history.txt")
 count_dict = {}
 for line in f.readlines():
@@ -10,14 +12,24 @@ for line in f.readlines():
     #print(line)
     count = count_dict.setdefault(line, 0)
     count += 1
-    count_dict[line] = count
-    
+    count_dict[line] = count   
 sorted_count_dict = sorted(count_dict.iteritems(), key=operator.itemgetter(1), reverse=True)
 f2=file("cmd.txt", "w") #打开2.txt文件，用于输出
-for item in sorted_count_dict:
-    #print "%s,%d" % (item[0], item[1])
-                                    
+for item in sorted_count_dict:                                 
     f2.write(str(item[0]) + "\t")
     f2.write(str(item[1])+ "\n") 
-
 f2.close
+
+f = open("cmd.txt")
+cmd=[]
+for line in f.readlines():
+    cmd.append(line)   
+cmd.sort()
+
+
+file = xlwt.Workbook()
+table = file.add_sheet('sheet1')
+for i, j in enumerate(cmd):
+    print i,j
+    table.write(i,0,j)
+file.save('demo.xls')
